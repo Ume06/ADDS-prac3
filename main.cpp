b45bc5350db2569d6d2b8898092bd85070fd5c8a
Jack Stewart Duggan <a1769726@emb106w004.ad.adelaide.edu.au>
2024-03-14 14:53:24 +1030

    - Add failure case to reverser

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
   int value = 678;
   Reverser test;
-  int rvalue = test.reverseDigit(10);
+  int rvalue = test.reverseDigit(-10);
   cout << rvalue << endl;
   cout << test.reverseString("Jack") << endl;
 }
